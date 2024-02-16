786460abdd10e1ff3c7773eda0e326ef8369b5f2
tyler <mastrangelo.tyler@gmail.com>
2024-02-16 09:33:30 -0500

    new tweet page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Pressable } from 'react-native';
 import Tweet from '@/components/tweet';
 import tweets from '@/assets/data/tweets';
+import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 export default function TabOneScreen() {
   return (
@@ -8,6 +10,15 @@ export default function TabOneScreen() {
       <FlatList 
         data={tweets} 
         renderItem={({item}) => <Tweet tweet={item} /> } />
+
+      <Link href="/new-tweet" asChild>
+        <Entypo 
+          name="plus" 
+          size={28} 
+          color="white"
+          style={styles.floatingButton}
+        />
+      </Link>
     </View>  
   );
 }
@@ -16,5 +27,26 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: 'white',
-  }
+  },
+  floatingButton: {
+    backgroundColor: '#1C9BF0',
+    borderRadius: 25,
+    padding: 15,
+
+    position: 'absolute',
+    right: 15,
+    bottom: 15,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+
+    overflow: 'hidden',
+  },
 });
