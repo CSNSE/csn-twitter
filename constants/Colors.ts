cd578bb832d2be0dc995aa13e33793a059d3e378
tyler <mastrangelo.tyler@gmail.com>
2024-04-11 12:13:58 -0400

    base color scheme #22

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,5 @@
-const tintColorLight = '#024b8f';
-const tintColorDark = '#fff';
+const tintColorLight = '#0a84ff';
+const tintColorDark = '#0a84ff';
 
 export default {
   light: {
@@ -8,6 +8,7 @@ export default {
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
+    royalBlue: '#4169e1', // Example of a royal blue color for school theme
   },
   dark: {
     text: '#fff',
@@ -15,5 +16,6 @@ export default {
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
+    royalBlue: '#4169e1',
   },
 };
